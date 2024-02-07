30073dc4664da0d4dc8fd9cf2ce854d66c3459e9
BiagioCaputo <Scebiagio98@gmail.com>
2024-02-07 23:24:03 +0100

    the response of the API is now written o the main, finished Milestone 1

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,13 +1,13 @@
 <script>
 import {store} from './../data/store'
 export default {
-  name: 'AppMain',
-  components: {},
-  data(){
-    return{
-        store,
-    }
-  }
+    name: 'AppMain',
+    components: {},
+    data(){
+        return{
+            store,
+        }
+    },
 }
 
 
@@ -15,7 +15,19 @@ export default {
 
 <template>
     <main>
-        
+        <div id="movie-section" class="row-container py-3">
+            <div class="row row-cols-2 row-cols-md-3 row-cols-xl-5 row-cols-xxl-6  gap-4 ">
+                <div class="col" v-for="movie in store.movies">
+                    <ul>
+                        <li>{{movie.title}}</li>
+                        <li>{{movie.originalTitle}}</li>
+                        <li>{{movie.ratings}}</li>
+                        <li>{{movie.language}}</li>
+                    </ul>
+                    
+                </div>
+            </div>
+        </div>
          
     </main>
 
